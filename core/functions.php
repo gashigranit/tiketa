1ebb630712540969da7aa0b55c9da49dc7e7232f
Granit Gashi <inst12mat.app@gmail.com>
2017-11-29 17:21:28 +0100

    Connect with MySQL database

--- core/functions.php
+++ core/functions.php
@@ -1,40 +1,20 @@
 <?php
-
-$films = [
-	[
-	  "id" => 1,
-	  "name" => "Wonder Woman",
-	  "description" => "Before she was Wonder Woman (Gal Gadot), she was Diana, princess of the Amazons, trained to be an unconquerable warrior. Raised on a sheltered island paradise...",
-	  "rating" => "5/5",
-	  "year" => 2017
-	],
-	[
-	  "id" => 2,
-	  "name" => "Blade Runner",
-	  "description" => "Deckard (Harrison Ford) is forced by the police Boss (M. Emmet Walsh) to continue his old job as Replicant Hunter. His assignment: eliminate four escaped Replicants from the colonies who have returned to Earth...",
-	  "rating" =>  "5/5",
-	  "year" => 2017
-	],
-	[
-	  "id" => 3,
-	  "name" => "Justice League",
-	  "description" => "Fueled by his restored faith in humanity and inspired by Superman's selfless act, Bruce Wayne enlists newfound ally Diana Prince to face an even greater threat...",
-	  "rating" =>  "5/5",
-	  "year" => 2017
-	],
-	[
-	  "id" => 4,
-	  "name" => "Dunkirk",
-	  "description" => "In May 1940, Germany advanced into France, trapping Allied troops on the beaches of Dunkirk...",
-	  "rating" =>  "5/5",
-	  "year" => 2017
-	],
-];
-
+include "protected/config.php";
 function get_all_films() {	
 
-	// TODO: Get films from database
-	global $films;
+	$con = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+	if(mysqli_connect_error()) {
+		echo "Error connecting to database: " . mysqli_connect_error();
+		exit();
+	}
+	
+	$sql = "SELECT * FROM films";
+	$films = [];
+	$result = mysqli_query($con, $sql);
+	
+	while($row = $result->fetch_assoc()) {
+		$films[] = array("id" => $row["id"], "name" => $row["name"], "description" => $row["description"], "rating" => $row["rating"], "year" => $row["year"]);
+	}
 	return $films;
 }
 
@@ -52,16 +32,17 @@ function get_film_by_name($name) {
 }
 
 function get_film_by_id($id) {
-	// TODO: Get film from database
-	
-	global $films;	
-	$found_film = [];
-	foreach($films as $film) {
-		if($film["id"] == $id) {
-			$found_film = $film;
-		}
+	$con = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+	if(mysqli_connect_error()) {
+		echo "Error connecting to database: " . mysqli_connect_error();
+		exit();
 	}
-	return $found_film;	
+	
+	$sql = "SELECT * FROM films WHERE id = $id";
+	$result = mysqli_query($con, $sql);	
+	$row = $result->fetch_assoc();
+	$film = array("id" => $row["id"], "name" => $row["name"], "description" => $row["description"], "rating" => $row["rating"], "year" => $row["year"]);
+	return $film;	
 }
 
 function update_film($film) {
