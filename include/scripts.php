020e8082b884a8a63f6e26ed4722a93b4fb670b5
Granit Gashi <grannit.g@gmail.com>
2018-01-03 10:52:28 +0100

    Fix bootstrap js link

--- include/scripts.php
+++ include/scripts.php
@@ -1,3 +1,3 @@
 <!-- Bootstrap core JavaScript -->
 <script src="include/js/jquery/jquery.min.js"></script>
-<script src="include/js/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="include/bootstrap/js/bootstrap.bundle.min.js"></script>
